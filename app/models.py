5311dd020d7cea761461b5c46d7a201c70aa41f4
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-08 21:17:24 +0300

    :ft define role in the user model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
+from enum import unique
 from . import db
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(100), unique=True, nullable=False)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     
     def __repr__(self): 
