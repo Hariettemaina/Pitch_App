6a712ea5cf777053f5f138ae2a0f0c63c290f394
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-09 00:44:09 +0300

    define the user model

--- app/models.py
+++ app/models.py
@@ -1,11 +1,27 @@
 
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    #role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+
+
+
+class User(UserMixin, db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    about = db.Column(db.String(255))
+    avatar = db.Column(db.String())
+    password_encrypt=db.Column(db.String(128))
+    pitches = db.relationship('Pitches', backref='user', lazy='dynamic')
+    likes = db.relationship('UpVote', backref='user', lazy='dynamic')
+    comments = db.relationship('Comments', backref='comments', lazy='dynamic')
+
+    
     
     def __repr__(self): 
         return f'User {self.username}'
--- run.py
+++ run.py
@@ -2,12 +2,18 @@ from multiprocessing import Manager
 from xmlrpc.client import Server
 from app import create_app,db
 from app.models import User
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = create_app('dev')
 
 manager = Manager(app)
 manager.add_command('sever', Server)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
 
 
 @manager.shell
