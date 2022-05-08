077362d8207134d1859baf5d0b969dd806be4441
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-09 02:01:33 +0300

    ft:Flask-Migrate

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 from multiprocessing import Manager
 from xmlrpc.client import Server
 from app import create_app,db
@@ -5,20 +6,14 @@ from app.models import User
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = create_app('dev')
 
-manager = Manager(app)
-manager.add_command('sever', Server)
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
 
-
-@manager.shell
-def make_shell_context():
-    return dict(app = app, db = db, User = User)
 
 
 
